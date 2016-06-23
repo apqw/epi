1676b4e710ed9bf9e449a02d8f5a04d7ead53ae3
aaaaaaaa <a@a.jp>
2016-06-23 18:38:19 +0900

    Vec

--- AMLib/Vec.h
+++ AMLib/Vec.h
@@ -5,16 +5,16 @@
 
 #define DEFUFUNC(F,fname) \
 template<typename T,class = typename VecValidType1<T>>\
-VUnary<T, F, T::size> fname(const T& l){\
-	return VUnary<T, F, T::size>(l);\
+VExprUnary<T, F, T::size> fname(const T& l){\
+	return VExprUnary<T, F, T::size>(l);\
 }
 
 #define DEFUOP(F,op) DEFUFUNC(F,operator##op)
 
 #define DEFBFUNC(F,fname)\
 template<typename T,typename U,class = typename VecValidType2<T,U>>\
-VExpr<T, F, U, T::size> fname(const T& l, const U& r) {\
-	return VExpr<T, F, U, T::size>(l, r);\
+VExprBinary<T, F, U, T::size> fname(const T& l, const U& r) {\
+	return VExprBinary<T, F, U, T::size>(l, r);\
 }
 
 #define DEFBOP(F,op) DEFBFUNC(F,operator##op)
@@ -35,54 +35,94 @@ namespace aml {
 
 	template<class T, class U>
 	using VecValidType2 = typename std::enable_if<std::is_base_of<VecBase<T::size>, T>::value && std::is_base_of<VecBase<U::size>, U>::value && T::size == U::size>::type;
+	
+	template<class T,size_t N>
+	using IsVec = typename std::enable_if<T::size == N&&std::is_base_of<VecBase<T::size>, T>::value>::type;
 
-	
-	
-	template <class L, template<class,class> class Op, class R,std::size_t N>
-	class VExpr:public VecBase<N> {
+	template<class T, size_t N>
+	using IsNotVec = typename std::enable_if<T::size != N||!std::is_base_of<VecBase<T::size>, T>::value>::type;
+
+	template<class T>
+	using IsPtrAccessible = decltype(*std::declval<T&>(), void(), ++std::declval<T&>(), void());
+
+
+
+	template <class L, template<class, class> class Op, class R, std::size_t N>
+	class VExprBinary :public VecBase<N> {
 		const L& l_;
 		const R& r_;
 	public:
-		VExpr(const L& l, const R& r)
+		VExprBinary(const L& l, const R& r)
 			: l_(l), r_(r) {}
 
 		auto operator[](std::size_t i) const
 		{
-			return Op<decltype(l_[i]),decltype(r_[i])>::Apply(l_[i], r_[i]);
+			return Op<decltype(l_[i]), decltype(r_[i])>::Apply(l_[i], r_[i]);
 		}
+	};
 
+	template <class L, class lmbd, class R, std::size_t N>
+	class VExprBinary_lambda :public VecBase<N> {
+		const L& l_;
+		const R& r_;
+		const lmbd fn;
+	public:
+		VExprBinary_lambda(const L& l, const R& r, const lmbd& f)
+			: l_(l), r_(r), fn(f) {}
 
+		auto operator[](std::size_t i) const
+		{
+			return fn(l_[i], r_[i]);
+		}
 	};
 
 	template <class T, template<class> class UOp, std::size_t N>
-	class VUnary :public VecBase<N> {
+	class VExprUnary :public VecBase<N> {
 		const T& v_;
 	public:
-		VUnary(const T& l)
+		VExprUnary(const T& l)
 			: v_(l) {}
 
 		auto operator[](std::size_t i) const
 		{
 			return UOp<decltype(v_[i])>::Apply(v_[i]);
 		}
-
-
 	};
 
-	template<typename T,std::size_t N>
-	class Vec:public VecBase<N>
+	template <class T, class lmbd, std::size_t N>
+	class VExprUnary_lambda :public VecBase<N> {
+		const T& v_;
+		const lmbd fn;
+	public:
+		VExprUnary_lambda(const T& l, const lmbd& f)
+			: v_(l), fn(f) {}
+
+		auto operator[](std::size_t i) const
+		{
+			return fn(v_[i]);
+		}
+	};
+
+	template<typename T, std::size_t N>
+	class Vec :public VecBase<N>
 	{
 	private:
+		struct VType{};
 		std::array<T, N> _value;
 
-		template<typename itr_begin,std::size_t... I>
+		template <class V, std::size_t... I>
+		explicit Vec(VType,const V& vex, std::index_sequence<I...>) :_value{ vex[I]... } {};
+
+		template<typename itr_begin, std::size_t... I, class = IsPtrAccessible<itr_begin>>
 		explicit Vec(const itr_begin& arr_itr_begin, std::index_sequence<I...>) :_value{ *(arr_itr_begin + I)... } {};
 
-		template <class L, template<class, class> class Op, class R, std::size_t... I>
-		explicit Vec(const VExpr<L, Op, R,N>& vex, std::index_sequence<I...>) :_value{vex[I]...} {};
+		
 	public:
-		template <class L, template<class, class> class Op, class R>
-		Vec(const VExpr<L, Op, R,N>& vex) :Vec(vex, std::make_index_sequence<N>()) {};
+		template <class V, class = typename IsVec<V,N>>
+		Vec(const V& vex) :Vec(VType(),vex, std::make_index_sequence<N>()) {};
+
+		template <class V, class = typename IsVec<V, N>>
+		Vec(V&& vex) :Vec(VType(), vex, std::make_index_sequence<N>()) {};
 
 		Vec(const std::array<T, N>& arr) :_value(arr) {};
 
@@ -103,7 +143,7 @@ namespace aml {
 
 		template<class E>
 		Vec& operator=(const E& r) {
-			for (std::size_t i = 0; i < N;i++) {
+			for (std::size_t i = 0; i < N; i++) {
 				_value[i] = r[i];
 			}
 			return *this;
@@ -115,7 +155,7 @@ namespace aml {
 			return *this;
 		}
 
-		~Vec() {};
+		~Vec() {}
 	};
 
 
@@ -168,6 +208,7 @@ namespace aml {
 				return -l;
 			}
 		};
+		/*
 
 		template<class CallableClass,typename T>
 		struct _UFunc_Impl {
@@ -178,11 +219,23 @@ namespace aml {
 				return f(l);
 			}
 		};
+		*/
 
 		template<class CallableClass>
 		struct _UFunc {
 			template<class T>
+			struct _UFunc_Impl {
+			private:
+				static CallableClass f;
+			public:
+				static auto Apply(const T& l) {
+					return f(l);
+				}
+			};
+			/*
+			template<class T>
 			using value = typename _UFunc::template _UFunc_Impl<CallableClass, T>;
+			*/
 		};
 
 	}
@@ -194,7 +247,7 @@ namespace aml {
 
 	DEFBOP(Ops::_Mul, *);
 
-	DEFBOP(Ops::_Div, /);
+	DEFBOP(Ops::_Div, / );
 
 	DEFUOP(Ops::_Negate, -);
 
@@ -211,10 +264,21 @@ namespace aml {
 	bool operator!=(const T& l, const U& r) {
 		return !(l == r);
 	}
-	
+	/*
 	template<class CallableClass,typename T,class = typename VecValidType1<T>>
-	VUnary<T, (Ops::_UFunc<CallableClass>::value), T::size> Apply(const T& l) {
-			return VUnary<T, Ops::_UFunc<CallableClass>::value<T>, T::size>(l);
+	VExprUnary<T, (Ops::_UFunc<CallableClass>::value), T::size> Apply(const T& l) {
+			return VExprUnary<T, Ops::_UFunc<CallableClass>::value<T>, T::size>(l);
 	}
-	
+	*/
+
+	template<typename T, class lmbd, class = typename VecValidType1<T>>
+	VExprUnary_lambda<T, lmbd, T::size> Apply(const T& l, lmbd&& fn) {
+		return VExprUnary_lambda<T, lmbd, T::size>(l, fn);
+	}
+
+	template<typename T, typename U, class lmbd, class = typename VecValidType2<T, U>>
+	VExprBinary_lambda<T, lmbd, U, T::size> Apply(const T& l, const U& m, lmbd&& fn) {
+		return VExprBinary_lambda<T, lmbd, U, T::size>(l, m, fn);
+	}
+
 }
--- AMLib/main.cpp
+++ AMLib/main.cpp
@@ -7,9 +7,14 @@ int main() {
 	for (int i = 0; i < 10000000; i++) {
 		aaa += c*aaa+aaa / aaa;
 	}
+	
 	auto a1 = (c + aaa*aaa*aaa*aaa + aaa / c)[0];
 	auto a2 = (aaa*aaa*aaa*aaa + aaa / c)[0];
-
-	printf("%lf %lf %lf %lf %d \n", aaa[0], aaa[1],a1,a2,a1==a2);
+	Vec<double, 3> dd(aaa*aaa);
+	Vec<double, 3> ap = { 3.14/4,3.14 / 2,3.14 / 4 };
+	auto&& a = [](double oo) {return sin(oo); };
+	auto a4 = Apply(ap, a);
+	auto a5 = Apply(ap, ap, [](double _a, double _b) {return _a + 10 + _b; });
+	printf("%lf %lf %lf %lf %d \n", a5[0], aaa[1],a1,a2,a1==a2);
 	system("pause");
 }
